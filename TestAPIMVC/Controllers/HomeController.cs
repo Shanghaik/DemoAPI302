6579dafb3b97613937809b2fc32a571a171ea8d5
shanghaik168@gmail.com <Shanghaik1998>
2023-05-17 16:02:34 +0700

    áddsaddas

--- TestAPIMVC/Controllers/HomeController.cs
+++ TestAPIMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using TestAPIMVC.Models;
 
@@ -17,9 +18,24 @@ namespace TestAPIMVC.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public IActionResult Privacy()
         {
+            return View();
+        }
+        public async Task<IActionResult> PrivacyAsync(double distance, double period)
+        {
+            string apiUrl =
+                $"https://localhost:7251/WeatherForecast/" +
+                $"get-average-pace?distance={distance}&period={period}";
+            var httpClient = new HttpClient(); // tạo ra để callApi
+            var response = await httpClient.GetAsync(apiUrl);// Lấy dữ liệu ra từ API URL
+                                                             // Lấy dữ liệu Json trả về từ Api được call dạng string
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kqua trả về từ API dạng Json
+            // Đọc từ string Json vừa thu được sang double
+            double pace = JsonConvert.DeserializeObject<double>(apiData);
+            ViewData["result"] = "Tốc độ chạy trung bình của bạn là :"+pace;
             return View();
         }
 
